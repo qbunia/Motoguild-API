acab956659cbd5a18d27a90d49b60b7f85deda1f
Michal Prosniak <michalprosniak@gmail.com>
2022-08-04 11:35:51 +0200

    Stops bug fix

--- MotoGuild API/Models/Route/RouteDto.cs
+++ MotoGuild API/Models/Route/RouteDto.cs
@@ -1,4 +1,5 @@
 ﻿using MotoGuild_API.Models.Post;
+using MotoGuild_API.Models.Stops;
 
 namespace MotoGuild_API.Models.Route
 {
@@ -7,7 +8,7 @@ namespace MotoGuild_API.Models.Route
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<Stops.Stops>? Stops { get; set; }
+        public ICollection<StopDto>? Stops { get; set; }
         public string StartPlace { get; set; }
         public string EndingPlace { get; set; }
         public ICollection<PostDto> Posts { get; set; } = new List<PostDto>();
