35cd0f35bd4d068fcd16f24a62db333005968968
Kamil Kornek <kamil.kornek96@gmail.com>
2022-08-18 13:12:31 +0200

    Route stops controller done (test needed)

---
+++ MotoGuild API/Models/Stop/UpdateStopDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MotoGuild_API.Models.Stops
+{
+    public class UpdateStopDto
+    {
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public string Description { get; set; }
+
+    }
+}
