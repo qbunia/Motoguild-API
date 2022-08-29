a829ac5223d649677f6bb71f83771ea2a527e29e
Mateusz Karpowicz <mateuszkarpowicz@gmail.com>
2022-08-29 14:07:25 +0200

    User rebuilded to mapper

--- MotoGuild API/Controllers/User/UserController.cs
+++ MotoGuild API/Controllers/User/UserController.cs
@@ -1,10 +1,11 @@
-﻿
+﻿using AutoMapper;
 using Data;
 using Domain;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MotoGuild_API.Models.User;
+using MotoGuild_API.Repository.Interface;
 
 namespace MotoGuild_API.Controllers
 {
@@ -13,114 +14,60 @@ namespace MotoGuild_API.Controllers
     [EnableCors("AllowAnyOrigin")]
     public class UserController : ControllerBase
     {
-        private MotoGuildDbContext _db;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UserController(MotoGuildDbContext dbContext)
+        public UserController(IUserRepository userRepository, IMapper mapper)
         {
-            _db = dbContext;
+            _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public IActionResult GetUsers()
         {
-            List<User> users = _db.Users.ToList();
-            var UsersDto = GetUsersDtos(users);
-            return Ok(UsersDto);
+            var users = _userRepository.GetAll();
+            return Ok(_mapper.Map<List<UserDto>>(users));
         }
 
         [HttpGet("{id:int}", Name = "GetUser")]
         public IActionResult GetUser(int id, [FromQuery] bool selectedData = false)
         {
-            var user = _db.Users.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            var UserDto = GetUserDto(user);
-            return Ok(UserDto);
+            var user = _userRepository.Get(id);
+            if (user == null) return NotFound();
+            return Ok(_mapper.Map<UserDto>(user));
         }
 
         [HttpPost]
         public IActionResult CreateUser([FromBody] CreateUserDto createUserDto)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var user = SaveUserToDataBase(createUserDto);
-            var userDto = GetUserDto(user);
+            var user = _mapper.Map<User>(createUserDto);
+            _userRepository.Insert(user);
+            _userRepository.Save();
+            var userDto = _mapper.Map<UserDto>(user);
             return CreatedAtRoute("GetUser", new { id = userDto.Id }, userDto);
         }
 
         [HttpDelete("{id:int}")]
         public IActionResult DeleteUser(int id)
         {
-            var user = _db.Users.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _db.Remove(user);
-            _db.SaveChanges();
+            var user = _userRepository.Get(id);
+            if (user == null) return NotFound();
+            _userRepository.Delete(id);
+            _userRepository.Save();
             return Ok();
         }
 
         [HttpPut("{id:int}")]
         public IActionResult UpdateUser(int id, [FromBody] UpdateUserDto updateUserDto)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var user = _db.Users.FirstOrDefault(i => i.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            UpdateUserData(user, updateUserDto);
+            var user = _userRepository.Get(id);
+            if (user == null) return NotFound();
+            _mapper.Map(updateUserDto, user);
+            _userRepository.Update(user);
+            _userRepository.Save();
             return NoContent();
         }
 
-        private void UpdateUserData(User user, UpdateUserDto updateUserDto)
-        {
-            user.UserName = updateUserDto.UserName;
-            user.Email = updateUserDto.Email;
-            user.PhoneNumber = updateUserDto.PhoneNumber;
-            _db.SaveChanges();
-        }
-
-        private User SaveUserToDataBase(CreateUserDto createUserDto)
-        {
-            User user = new User()
-            {
-                UserName = createUserDto.UserName,
-                Email = createUserDto.Email,
-                PhoneNumber = createUserDto.PhoneNumber,
-                Rating = 0
-            };
-            _db.Users.Add(user);
-            _db.SaveChanges();
-            return user;
-        }
-
-
-        private List<UserDto> GetUsersDtos(List<User> users)
-        {
-            var usersDtos = new List<UserDto>();
-            foreach (var user in users)
-            {
-                usersDtos.Add(new UserDto() { Email = user.Email, Rating = user.Rating, Id = user.Id, UserName = user.UserName });
-            }
-            return usersDtos;
-        }
-
-        private UserDto GetUserDto(User user)
-        {
-            var userDto = new UserDto()
-                { Email = user.Email, Rating = user.Rating, Id = user.Id, UserName = user.UserName };
-            return userDto;
-        }
     }
 }
--- MotoGuild API/Program.cs
+++ MotoGuild API/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddScoped<IGroupRepository, GroupRepository>();
 builder.Services.AddScoped<IRideRepository, RideRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
