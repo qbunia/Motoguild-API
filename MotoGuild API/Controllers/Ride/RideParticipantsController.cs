a535fd70f3a640f029740003b328333c416b98da
Kamil Kornek <kamil.kornek96@gmail.com>
2022-08-30 09:19:39 +0200

    ride participants controller done (need to be tested)

--- MotoGuild API/Controllers/Ride/RideParticipantsController.cs
+++ MotoGuild API/Controllers/Ride/RideParticipantsController.cs
@@ -1,132 +1,86 @@
-using System.Runtime.ExceptionServices;
+using AutoMapper;
 using Data;
 using Domain;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MotoGuild_API.Models.User;
-
+using MotoGuild_API.Repository.Interface;
 
 namespace MotoGuild_API.Controllers;
-
 
 [ApiController]
 [Route("api/rides/{rideId:int}/participants")]
 [EnableCors("AllowAnyOrigin")]
 public class RideParticipantsController : ControllerBase
 {
-    private MotoGuildDbContext _db;
+    private readonly IRideParticipantsRepository _rideParticipantsRepository;
+    private readonly IMapper _mapper;
 
-    public RideParticipantsController(MotoGuildDbContext db)
+    public RideParticipantsController(IRideParticipantsRepository rideParticipantsRepository, IMapper mapper)
     {
-        _db = db;
+        _rideParticipantsRepository = rideParticipantsRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
     public IActionResult GetRideParticipants(int rideId)
     {
-        var participants = _db.Rides.Include(r => r.Participants)
-            .Where(r => r.Id == rideId).Select(r => r.Participants).First().ToList();
-        var participantsDto = GetRideParticipantsDtos(participants);
-        return Ok(participantsDto);
+        var participants = _rideParticipantsRepository.GetAll(rideId);
+        if (participants == null)
+        {
+            return NotFound();
+        }
+        return Ok(_mapper.Map<List<UserDto>>(participants));
     }
 
     [HttpGet("{id:int}", Name = "GetRideParticipant")]
     public IActionResult GetRideParticipant(int rideId, int id)
     {
-        var ride = _db.Rides.Include(r => r.Participants)
-            .FirstOrDefault(r => r.Id == rideId);
-        if (ride == null)
-        {
-            return NotFound();
-        }
-
-        var participant = ride.Participants.FirstOrDefault(p => p.Id == id);
+        var participant = _rideParticipantsRepository.Get(rideId, id);
         if (participant == null)
         {
             return NotFound();
         }
-
-        var participantDto = GetRideParticipantDto(participant);
-        return Ok(participantDto);
+        return Ok(_mapper.Map<UserDto>(participant));
     }
 
     [HttpPost("{id:int}")]
     public IActionResult AddRideParticipantByUserId(int rideId, int id)
     {
-        var ride = _db.Rides.Include(r => r.Participants)
-            .FirstOrDefault(p => p.Id == rideId);
-        if (ride == null)
+        if (!_rideParticipantsRepository.RideExist(rideId) || !_rideParticipantsRepository.UserExits(id))
         {
             return NotFound();
         }
 
-        var participant = _db.Users.FirstOrDefault(p => p.Id == id);
-        if (participant == null)
+        if (_rideParticipantsRepository.UserInRide(rideId, id))
         {
-            return NotFound();
+            return BadRequest();
         }
-
-        AddRideParticipantToRide(ride, participant);
-        var participantDto = GetRideParticipantDto(participant);
-        return Ok(participantDto);
-
+        _rideParticipantsRepository.AddParticipantByUserId(rideId, id);
+        _rideParticipantsRepository.Save();
+        var user = _rideParticipantsRepository.GetUser(id);
+        var userDto = _mapper.Map<UserDto>(user);
+        return CreatedAtRoute("GetRideParticipant", new { id = userDto.Id, rideId = rideId }, userDto);
     }
 
-    private void AddRideParticipantToRide(Domain.Ride ride, User participant)
-    {
-        ride.Participants.Add(participant);
-        _db.SaveChanges();
-    }
 
-    private UserDto GetRideParticipantDto(User participant)
-    {
-        var participantDto = new UserDto()
-        {
-            Email = participant.Email,
-            Id = participant.Id,
-            Rating = participant.Rating,
-            UserName = participant.UserName
-        };
-        return participantDto;
-    }
-
-    private List<UserDto> GetRideParticipantsDtos(List<User> participants)
-    {
-        var participantsDtos = new List<UserDto>();
-        foreach (var participant in participants)
-        {
-            participantsDtos.Add(new UserDto()
-            {
-                Email = participant.Email, Rating = participant.Rating, Id = participant.Id,
-                UserName = participant.UserName
-            });
-        }
-
-        return participantsDtos;
-    }
-    
     [HttpDelete("{id:int}")]
     public IActionResult DeleteRideParticipantByUserId(int rideId, int id)
     {
-        var ride = _db.Rides.Include(r => r.Participants)
-            .FirstOrDefault(p => p.Id == rideId);
-        if (ride == null)
+        if (!_rideParticipantsRepository.RideExist(rideId) || !_rideParticipantsRepository.UserExits(id))
         {
             return NotFound();
         }
-        var participant = _db.Users.FirstOrDefault(p => p.Id == id);
-        if (participant == null)
+
+        if (!_rideParticipantsRepository.UserInRide(rideId, id))
         {
             return NotFound();
         }
-        DeleteRideParticipantFromRide(ride, participant);
+        _rideParticipantsRepository.DeleteParticipantByUserId(rideId, id);
+        _rideParticipantsRepository.Save();
+
         return NoContent();
     }
-    
-    private void DeleteRideParticipantFromRide(Domain.Ride ride, User participant)
-    {
-        ride.Participants.Remove(participant);
-        _db.SaveChanges();
-    }
+
 }
