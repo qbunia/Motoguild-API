ce32eadb4791cd320009ffb26988e11ee0f07a57
Michal Prosniak <michalprosniak@gmail.com>
2022-08-31 14:41:23 +0200

    group get by id bug fix

--- MotoGuild API/Repository/Interface/Group/IGroupRepository.cs
+++ MotoGuild API/Repository/Interface/Group/IGroupRepository.cs
@@ -1,10 +1,12 @@
 ﻿using Domain;
+using MotoGuild_API.Helpers;
 
 namespace MotoGuild_API.Repository.Interface
 {
     public interface IGroupRepository : IDisposable
     {
-        IEnumerable<Group> GetAll();
+        IEnumerable<Group> GetAll(PaginationParams @params);
+        int TotalNumberOfGroups();
         Group Get(int groupId);
         void Insert(Group group);
         void Delete(int groupId);
