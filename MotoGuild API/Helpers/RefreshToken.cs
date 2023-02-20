d68951a07ade39a610f59afeea7d359ebb274fa5
qbunia <dsawowlol@gmail.com>
2023-02-20 21:44:45 +0100

    Update RefreshToken.cs

--- MotoGuild API/Helpers/RefreshToken.cs
+++ MotoGuild API/Helpers/RefreshToken.cs
@@ -2,8 +2,8 @@
 {
     public class RefreshToken
     {
-        public string Token { get; set; } = string.Empty;
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime Expires { get; set; }
+        public string Token { get; set; } = string.Empty; // czemu nie null?
+        public DateTime Created { get; set; } = DateTime.Now; //Błąd, ustawienie daty jest zależne od lokalnych ustawień serwera. To trzeba poprawić np. na polska datę zawsze albo UTC
+        public DateTime Expires { get; set; } //Jest to typu DateTime więc jak ktos zainicjuje klase RefreshToken i nie ustawi Expires to bedzie to 0000-01-01. Chyba nie tego chcemy?
     }
 }
